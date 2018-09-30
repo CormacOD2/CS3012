dc27906cc0fe06f98f4029943cbe7e89f0c9fc27
Cormac <cormac.odwyer@hotmail.com>
2018-09-30 21:02:15 +0100

    Setting up local repo for project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS3012
 Repository for Software Engineering CS3012 assignments - Trinity College Year 3
+
+Input : handled by text file input , can alter the input by changing the file name at the top of main file to like to a separate file or edit the 'nodeInput' text file itelf
--- main.c
+++ main.c
@@ -1,38 +1,54 @@
-
 #include "binaryTree.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int main(int argc, char** argv) {
-    NODE *root = NULL;
-    char ch, buffer[32];
+char *FILE_NAME = "nodeInput.txt";
+struct node *root = NULL;
+void fileInput(){
+    char ch, buffer[1000],temp;
     int i=0;
     FILE *f;
-    
-    if((f = fopen("nodeInput.txt","r"))==NULL){
-        printf("File cannot be opened\n");
+    char *fileName ="";
+    int exitBool = 0;
+        while(exitBool == 0){
+        printf("Enter '1' for default binary tree, or '2' to run test input\n");
+        fflush(stdout);
+        scanf("%s",&temp);
+        if(temp == '1'){
+            fileName = FILE_NAME;
+            exitBool = 1;
+        }else if(temp <= '2'){
+            fileName = "testNodeInput.txt";
+            exitBool = 1;
+        }
+    }
+    if((f = fopen(fileName,"r"))==NULL){
+        printf("Error : File cannot be opened\n");
         exit(255);
     }
-    
-    while((ch = fgetc(f)) != EOF){
-        if(ch == ','){
+        while((ch = fgetc(f)) != EOF){
+        if(ch == ',' || ch == ' '|| ch == '\t'){
             int key = atoi(buffer);
-
             if(root == NULL){
                 root = insert(root,key);
             }
-            insert(root,key);
-            
+                        insert(root,key);
             bzero(buffer,32);
             i = 0;
+        }else if(ch == '-' && i == 0){
+            buffer[i] = ch;
+            i++;
         }else if(ch >= '0' && ch <= '9'){
             buffer[i] = ch;
             i++;
+        }else{
+            bzero(buffer,32);
         }
     }
+}
+int main(int argc, char** argv) {
+    fileInput();
     structure(root,0);
-
     return (0);
 }
 
